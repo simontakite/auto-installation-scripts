6f42cecad913ded52d10fe29ca64a871c82ee989
Simon Takite <simon.takite@gmail.com>
2015-02-03 17:11:15 +0100

    Update ubuntu-post-installation.sh

--- ubuntu-post-installation.sh
+++ ubuntu-post-installation.sh
@@ -1,25 +1,9 @@
 #!/bin/bash
 # -*- Mode: sh; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
 #
-# Authors:
-#   Sam Hewitt <hewittsamuel@gmail.com>
-#
 # Description:
 #   A post-installation bash script for Ubuntu (14.04)
 #
-# Legal Stuff:
-#
-# This script is free software; you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free Software
-# Foundation; version 3.
-#
-# This script is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-# details.
-#
-# You should have received a copy of the GNU General Public License along with
-# this program; if not, see <https://www.gnu.org/licenses/gpl-3.0.txt>
 
 echo ''
 echo '#-------------------------------------------#'
